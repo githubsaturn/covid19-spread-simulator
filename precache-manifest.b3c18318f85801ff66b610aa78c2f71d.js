9c2e693a1b6ba0bb6bb2165682caedd75eecc5c2
githubsaturn <githubsaturn@users.noreply.github.com>
2020-04-19 22:32:46 +0000

    Deploy to GitHub Pages

---
+++ precache-manifest.b3c18318f85801ff66b610aa78c2f71d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e91532a7cab00b8781c401e53c01c9b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75e8aa1b35c79076cfcc",
+    "url": "/static/css/2.1dec1cf8.chunk.css"
+  },
+  {
+    "revision": "1d300d24be74e7262d6f",
+    "url": "/static/css/main.6763b551.chunk.css"
+  },
+  {
+    "revision": "75e8aa1b35c79076cfcc",
+    "url": "/static/js/2.2924aa0e.chunk.js"
+  },
+  {
+    "revision": "a8fa0a685d8af2934ba8ade7768fed07",
+    "url": "/static/js/2.2924aa0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d300d24be74e7262d6f",
+    "url": "/static/js/main.60494155.chunk.js"
+  },
+  {
+    "revision": "2745afda71bd1472b974",
+    "url": "/static/js/runtime-main.5f585a09.js"
+  }
+]);
